3b9f0612a44aa2d5da3754cc4dee2c2e9f71c319
David A. Solin <david@joval.org>
2012-10-09 08:32:24 -0500

    Minor doc changes.

--- customize.mk
+++ customize.mk
@@ -1,10 +1,10 @@
-# Copyright (C) 2011 jOVAL.org.  All rights reserved.
+# Copyright (C) 2012 jOVAL.org.  All rights reserved.
 # This software is licensed under the AGPL 3.0 license available at http://www.joval.org/agpl_v3.txt
 #
 # INSTRUCTIONS:
 #
 # This file is intended to be modified to suit your build environment.
-# $(TOP) is defined to be this directory -- the top-level directory for the jOVAL source tree.
+# $(TOP) is defined to be this directory -- the top-level directory for the jWSMV source tree.
 #
 
 #
@@ -12,24 +12,3 @@
 #
 #JAVA_HOME=$(TOP)/../../tools/jdk160_26
 JAVA_HOME=$(TOP)/../../tools/jdk170_03
-
-#
-# ARCH defines the default distribution target architecture.  It should correspond to your
-# platform architecture.
-#
-#ARCH=x86
-ARCH=x64
-
-#
-# JRE32_HOME is the install path for a 32-bit JRE.
-# This does not have to be set if you will never create a 32-bit distribution.
-#
-#JRE32_HOME=$(TOP)/../../tools/jre160_31_x86
-JRE32_HOME=$(TOP)/../../tools/jre170_03_x86
-
-#
-# JRE64_HOME is the install path for a 64-bit JRE.
-# This does not have to be set if you will never create a 64-bit distribution.
-#
-#JRE64_HOME=$(TOP)/../../tools/jre160_31_x64
-JRE64_HOME=$(TOP)/../../tools/jre170_03_x64
